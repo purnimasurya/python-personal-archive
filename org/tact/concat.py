c2dde7af54fab5e68b21cf5a3bafa02316a88636
Purnima Suryaprakash <purnima1999.surya@gmail.com>
2019-01-26 20:30:46 +0530

    Sample files added by Purnima

---
+++ org/tact/concat.py
@@ -0,0 +1,6 @@
+a = "THANK YOU "
+b = "FOR "
+c = "GIVING US "
+d = "THIS OPPORTUNITY"
+e = a+b+c+d
+print(e)
---
+++ org/tact/forLoop.py
@@ -0,0 +1,5 @@
+friends = ['john', 'pat', 'gary', 'michael']
+j=1
+for i in friends :
+    print('iteration ',j,'and name ',i )
+    j=j+1
