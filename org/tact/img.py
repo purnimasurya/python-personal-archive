8e4fd71c2dc577174eff4c0ab11463b23afee644
Purnima Suryaprakash <purnima1999.surya@gmail.com>
2019-01-31 21:07:14 +0530

    image file pushed

---
+++ org/tact/img.py
@@ -0,0 +1,7 @@
+import os
+
+for root, dirs, files in os.walk("/Users/purnimasurya/imgtest"):  
+    for filename in files:
+        name,ext = os.path.splitext(filename)
+        if(ext=='.png' or ext=='.jpg'):
+            print(filename)
