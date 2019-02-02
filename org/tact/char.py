793cf8196af4ab44d8dcc384dd50525d8318e0af
Purnima Suryaprakash <purnima1999.surya@gmail.com>
2019-02-02 20:54:32 +0530

    proj files are pushed

--- org/tact/char.py
+++ org/tact/char.py
@@ -1,5 +1,14 @@
 import os
+
 f=open("guru99.txt", "r")
 if f.mode == 'r':
     contents =f.read()
     print(contents)
+    count = 0
+    for c in contents :
+        if c.isspace() != True:
+            count = count + 1
+    print(count)
+             
+                
+                            
--- org/tact/img.py
+++ org/tact/img.py
@@ -1,5 +1,4 @@
 import os
-
 for root, dirs, files in os.walk("/Users/purnimasurya/imgtest"):  
     for filename in files:
         name,ext = os.path.splitext(filename)
