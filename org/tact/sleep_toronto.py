5ba34bfd3bdb52ebb3c4bb39a760cbd842d12b12
Purnima Suryaprakash <purnima1999.surya@gmail.com>
2019-02-05 18:18:43 +0530

    SLEEP METHOD SAMPLE ADDED

---
+++ org/tact/sleep_toronto.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# the above line is to avoid 'SyntaxError: Non-UTF-8 code starting with' error
+
+'''
+Created on 
+
+Course work: 
+
+@author: Purnima
+
+Source:
+    
+'''
+
+# Import necessary modules
+ 
+
+import time
+
+def startpy():
+    
+    i=0
+    while (i<5): 
+        print ("Hello Toronto")
+        time.sleep(5)
+        i = i+1
+if __name__ == '__main__':
+  
+    startpy()
